4dac76b77a2e161a08fa431ed69051e95ac491f8
tigerplush <christopher.putz@corgibase.de>
2025-12-14 15:55:05 +0100

    prepared logging

---
+++ modules/log/include/Log.h
@@ -0,0 +1,15 @@
+#ifndef LOG_H_
+#define LOG_H_
+
+namespace Log
+{
+    #define Log(severity_level)
+    #define TRACE
+    #define DEBUG
+    #define INFO
+    #define WARNING
+    #define ERROR
+} // namespace Log
+
+
+#endif
---
+++ modules/log/include/Logger.h
@@ -0,0 +1,13 @@
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+namespace Log
+{
+    class Logger
+    {
+
+    };
+} // namespace Log
+
+
+#endif
